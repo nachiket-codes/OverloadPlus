4896a0ac6b0375b1321fb85f698fba1f62043db9
nachiket-codes <fitnessnachiket@gmail.com>
2025-08-12 17:41:49 +0530

    Update Splits.tsx

--- OP/frontend/app/src/components/FPages/Splits.tsx
+++ OP/frontend/app/src/components/FPages/Splits.tsx
@@ -7,19 +7,18 @@ import BackDrop from "../Backdrop";
 import AddSplitModal from "./AddSplitModal";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store";
-import { getSplits } from "../../features/splitSlice";
+import { deleteSplits, getSplits } from "../../features/splitSlice";
+import SplitModal from "./SplitModal";
+import { toast } from "react-toastify";
+import LoadingSpinner from "../LoadingSpinner";
 
 const Splits = () => {
     const dispatch = useDispatch<AppDispatch>();
-    const {splits, error, loading} = useSelector((state: RootState) => state.splits)
+    const {splits, error, loading} = useSelector((state: RootState) =>  state.splits)
     const maxDescLen = 50
     const [optionOpenId, setOptionOpenId] = useState<string>('')
     const [addMode, setAddMode] = useState<boolean>(false)
-    const ttData = [
-        {id: "12", name: "Session 1", description: "Abcd of the xyz and the qwerty keys Abcd of the xyz and the qwerty keys"},
-        {id: "13", name: "Session 2", description: "sfafdf of the gew and the asdfg keys"}
-    ]
-    // const ttData: any[] = []
+    const [openSplitId, setOpenSplitId] = useState<string>("")
 
     const unSelect = (e: React.FormEvent) => {
         e.preventDefault();
@@ -34,6 +33,23 @@ const Splits = () => {
     useEffect(()=>{
         dispatch(getSplits())
     }, [dispatch])
+
+    useEffect(()=>{
+        if (error) {
+            toast.error(error)
+        }
+    }, [error])
+
+    const deleteSplit = async (e: React.FormEvent, id: string) => {
+        e.preventDefault();
+        await dispatch(deleteSplits(id))
+        await dispatch(getSplits())
+        unSelect(e)
+    }
+
+    if (loading) {
+        return <LoadingSpinner/>
+    }
 
     return (
         <div className="w-full h-screen">
@@ -54,9 +70,10 @@ const Splits = () => {
                                 splits.map((item, idx) => {
                                     return (
                                     <li className="border-b border-primary p-2 pl-4 hover:border-b-2 flex relative">
-                                        <div className="flex-1 cursor-pointer hover:text-primary ">
+                                        <div className="flex-1 cursor-pointer hover:text-primary " onClick={()=>setOpenSplitId(item.id)}>
                                             <h1 className="text-xl mb-2">{item.name}</h1>
                                             <p className="text-[12px] text-gray-400">{item.description.length > maxDescLen ? item.description.slice(0, maxDescLen) + '...' : item.description}</p>
+                                            <p className="text-[12px] text-gray-400 opacity-0">{!item.description && 'placeholder'}</p>
                                         </div>
                                         <div className="flex items-center text-xl cursor-pointer transition-all hover:scale-[1.05] ease-in-out">
                                             <FontAwesomeIcon icon={faEllipsisVertical} onClick={()=>setOptionOpenId(item.id)}/>
@@ -65,7 +82,7 @@ const Splits = () => {
                                             optionOpenId === item.id && (
                                                 <div className="shadow-xl absolute right-0 top-2 p-2 rounded-md bg-white flex  z-50">
                                                     <ul className="p-2 flex flex-col gap-2">
-                                                        <li className="m-1 hover:text-red-500 cursor-pointer"> <FontAwesomeIcon icon={faTrash}/> Delete</li>
+                                                        <li className="m-1 hover:text-red-500 cursor-pointer"> <FontAwesomeIcon icon={faTrash} onClick={(e)=>deleteSplit(e, item.id)}/> Delete</li>
                                                         <li className="m-1 hover:text-blue-500 cursor-pointer"><FontAwesomeIcon icon={faCheck}/> Set as active</li>
                                                     </ul>
                                                     <div className="flex pt-2 cursor-pointer hover:text-red-500 hover:font-bold">
@@ -103,7 +120,15 @@ const Splits = () => {
                             <AddSplitModal closeFunc={()=>setAddMode(false)}/>
                             </div>
                         )}
-                </AnimatePresence>
+                        {
+                            openSplitId && (
+                                <div>
+                                <BackDrop unselectionFunc={()=>setOpenSplitId("")}/>
+                                <SplitModal id={openSplitId} closeFunc={()=>setOpenSplitId("")}/>
+                                </div>
+                            )
+                        }
+                    </AnimatePresence>
                 </div>
                 
             </div>
